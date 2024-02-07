e6d2f4841d3321badd4aca573defbd1aeb8fb0e8
Thomas E. Enebo <tom.enebo@gmail.com>
2024-02-07 15:11:39 -0500

    bleh

--- src/main/java/org/jruby/prism/ParserProviderPrism.java
+++ src/main/java/org/jruby/prism/ParserProviderPrism.java
@@ -9,7 +9,6 @@ import org.jruby.prism.builder.IRBuilderFactoryPrism;
 
 public class ParserProviderPrism implements ParserProvider {
     public Parser getParser(Ruby runtime) {
-        System.out.println("PAALAALAL");
         return new ParserPrism(runtime);
     }
 
