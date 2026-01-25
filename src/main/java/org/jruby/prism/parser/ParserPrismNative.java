b09461686d7588865d019607139574b5d956f73e
Charles Oliver Nutter <headius@headius.com>
2026-01-25 17:15:30 -0600

    Clean up unused imports
    The native and wasm versions were split off into separate classes,
    so most of these imports are only needed in the base class now.

--- src/main/java/org/jruby/prism/parser/ParserPrismNative.java
+++ src/main/java/org/jruby/prism/parser/ParserPrismNative.java
@@ -1,43 +1,7 @@
 package org.jruby.prism.parser;
 
-import org.jcodings.Encoding;
-import org.jcodings.specific.ISO8859_1Encoding;
-import org.jruby.ParseResult;
 import org.jruby.Ruby;
-import org.jruby.RubyArray;
-import org.jruby.RubyIO;
-import org.jruby.RubyInstanceConfig;
-import org.jruby.RubySymbol;
-import org.jruby.ext.coverage.CoverageData;
 import org.jruby.management.ParserStats;
-import org.jruby.parser.Parser;
-import org.jruby.parser.ParserManager;
-import org.jruby.parser.ParserType;
-import org.jruby.parser.StaticScope;
-import org.jruby.runtime.DynamicScope;
-import org.jruby.runtime.ThreadContext;
-import org.jruby.runtime.builtin.IRubyObject;
-import org.jruby.runtime.load.LoadServiceResourceInputStream;
-import org.jruby.util.ByteList;
-import org.jruby.util.CommonByteLists;
-import org.jruby.util.io.ChannelHelper;
-import org.prism.Nodes;
-import org.prism.Nodes.*;
-import org.prism.ParsingOptions;
-import org.prism.Prism;
-
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.jruby.api.Convert.asSymbol;
-import static org.jruby.lexer.LexingCommon.DOLLAR_UNDERSCORE;
-import static org.jruby.parser.ParserType.EVAL;
-import static org.jruby.parser.ParserType.MAIN;
 
 public class ParserPrismNative extends ParserPrismBase {
     private final ParserBindingPrism prismLibrary;
