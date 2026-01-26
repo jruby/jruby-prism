976b8b29de06ea7e1776bec15959dcbc28d9ac16
Charles Oliver Nutter <headius@headius.com>
2026-01-26 14:13:13 -0600

    Clean up wasm wrapper and use pure-WASM for now

--- src/main/java/org/jruby/prism/parser/ParserPrismWasm.java
+++ src/main/java/org/jruby/prism/parser/ParserPrismWasm.java
@@ -1,30 +1,11 @@
 package org.jruby.prism.parser;
 
-import org.jcodings.Encoding;
-import org.jcodings.specific.ISO8859_1Encoding;
-import org.jruby.*;
-import org.jruby.parser.Parser;
-import org.jruby.parser.ParserType;
-import org.jruby.parser.StaticScope;
-import org.jruby.runtime.DynamicScope;
-import org.jruby.runtime.ThreadContext;
-import org.jruby.runtime.builtin.IRubyObject;
-import org.jruby.util.ByteList;
-import org.jruby.util.CommonByteLists;
-import org.prism.Nodes.*;
-import org.prism.ParsingOptions;
+import org.jruby.Ruby;
 import org.prism.Prism;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import static org.jruby.api.Convert.asSymbol;
-import static org.jruby.lexer.LexingCommon.DOLLAR_UNDERSCORE;
-import static org.jruby.parser.ParserType.EVAL;
+import org.prism.PrismWASM;
 
 public class ParserPrismWasm extends ParserPrismBase {
-    private static final Prism prism = new Prism();
+    private static final Prism prism = new PrismWASM();
 
     public ParserPrismWasm(Ruby runtime) {
         super(runtime);
