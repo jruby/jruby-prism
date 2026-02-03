7d2598f57c37a55107c621736579a4af5873aa98
Charles Oliver Nutter <headius@headius.com>
2026-02-03 13:56:19 -0600

    Synchronize access to the Prism instance
    WASM is inherently single-threaded, so we cannot allow multiple
    threads to be parsing with a single chicory-prism instance at the
    same time. For now, we synchronize that access. A pool of instances
    or a soft thread-local could be used in the future if that becomes
    a bottleneck.

--- src/main/java/org/jruby/prism/parser/ParserPrismWasm.java
+++ src/main/java/org/jruby/prism/parser/ParserPrismWasm.java
@@ -2,16 +2,15 @@ package org.jruby.prism.parser;
 
 import org.jruby.Ruby;
 import org.prism.Prism;
-import org.prism.PrismWASM;
 
 public class ParserPrismWasm extends ParserPrismBase {
-    private static final Prism prism = new PrismWASM();
+    private final Prism prism = new Prism();
 
     public ParserPrismWasm(Ruby runtime) {
         super(runtime);
     }
 
-    protected byte[] parse(byte[] source, int sourceLength, byte[] metadata) {
+    protected synchronized byte[] parse(byte[] source, int sourceLength, byte[] metadata) {
         return prism.serialize(metadata, source, sourceLength);
     }
 }
